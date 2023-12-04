8357d37a5e9a01156c8f20bffcbf65c2b19038ab
CR7-bi <152921845+CR7-bi@users.noreply.github.com>
2023-12-04 15:18:07 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,2 @@
+music.play(music.createSoundEffect(WaveShape.Square, 5000, 0, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+scene.setBackgroundColor(5)
